7110a8764bd828e7eba528d9268d2735a09803b6
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:06:35 +0800

    Disable SELinux

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,3 +46,4 @@ BOARD_SEPOLICY_DIRS += $(DEVICE_COMMON_PATH)/sepolicy
 
 # inherit from the proprietary version
 include vendor/lge/g5-common/BoardConfigVendor.mk
+BOARD_BOOTCONFIG += androidboot.selinux=permissive
